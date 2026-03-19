8b4e1138fe0aaf815d4265480c9ca2094b3df86d
Igor Banovic <ibanovic07@gmail.com>
2026-03-19 08:33:40 +0100

    Odradjeni useri

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/api/users', [UserController::class, 'index']);
+Route::post('/api/users', [UserController::class, 'store']);
+Route::get('/api/users/{user}', [UserController::class, 'show']);
+Route::put('/api/users/{user}', [UserController::class, 'update']);
+Route::delete('/api/users/{user}', [UserController::class, 'destroy']);
