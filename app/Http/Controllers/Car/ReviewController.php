372e922d4c52e47e07f6308973b3d34a19fdcd21
Igor Banovic <ibanovic07@gmail.com>
2026-03-26 14:33:36 +0100

    Added ReviewController for user

--- app/Http/Controllers/Car/ReviewController.php
+++ app/Http/Controllers/Car/ReviewController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers\Car;
 
+use App\Models\Car;
+
 class ReviewController
 {
-
+    public function index(Car $car)
+    {
+        return $car->reviews;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,22 +3,23 @@
 use App\Http\Controllers\CarController as CarAdminController;
 use App\Http\Controllers\Car\CarController as CarController;
 use App\Http\Controllers\ReservationController;
-use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\ReviewController as ReviewAdminController;
+use App\Http\Controllers\Car\ReviewController as ReviewController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::apiResource('/reservations', ReservationController::class);
-    Route::apiResource('/reviews', ReviewController::class);
-    Route::apiResource('/users', UserController::class);
+    Route::get('/{car}/reviews', [ReviewController::class, 'index']);
 });
 //for everyone
 Route::get('/cars/{dates}/availableCars', [CarController::class, 'index']);
+Route::post('/register', [AuthController::class, 'createUser']);
+Route::post('/login', [AuthController::class, 'loginUser']);
 
 //for admin
 Route::apiResource('/cars', CarAdminController::class);
-
-Route::post('/register', [AuthController::class, 'createUser']);
-Route::post('/login', [AuthController::class, 'loginUser']);
+Route::apiResource('/users', UserController::class);
+Route::apiResource('/reservations', ReservationController::class);
+Route::apiResource('/reviews', ReviewAdminController::class);
